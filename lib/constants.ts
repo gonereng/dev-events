25075c42693bea3f610437b4490931b6afad8922
Roland Theisen <roland.theisen@outlook.com>
2026-02-02 13:57:06 +0100

    Initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,91 @@
+export interface EventItem {
+  title: string;
+  image: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
+}
+
+export const events: EventItem[] = [
+  {
+    title: "NVIDIA GTC 2026",
+    image: "https://images.unsplash.com/photo-1540575467063-178a50c2df87?w=800",
+    slug: "nvidia-gtc-2026",
+    location: "San Jose, CA",
+    date: "March 16-19, 2026",
+    time: "9:00 AM - 6:00 PM",
+  },
+  {
+    title: "DeveloperWeek 2026",
+    image: "https://images.unsplash.com/photo-1505373877841-8d25f7d46678?w=800",
+    slug: "developerweek-2026",
+    location: "San Jose, CA",
+    date: "February 18-20, 2026",
+    time: "8:30 AM - 7:00 PM",
+  },
+  {
+    title: "FOSDEM 2026",
+    image: "https://images.unsplash.com/photo-1475721027785-f74eccf877e2?w=800",
+    slug: "fosdem-2026",
+    location: "Brussels, Belgium",
+    date: "February 1-2, 2026",
+    time: "10:00 AM - 6:00 PM",
+  },
+  {
+    title: "GDC Festival of Gaming",
+    image: "https://images.unsplash.com/photo-1538481199705-c710c4e965fc?w=800",
+    slug: "gdc-2026",
+    location: "San Francisco, CA",
+    date: "March 9-13, 2026",
+    time: "9:00 AM - 8:00 PM",
+  },
+  {
+    title: "VueConf US 2026",
+    image: "https://images.unsplash.com/photo-1511578314322-379afb476865?w=800",
+    slug: "vueconf-us-2026",
+    location: "Atlanta, GA",
+    date: "May 19-21, 2026",
+    time: "9:00 AM - 5:30 PM",
+  },
+  {
+    title: "WeAreDevelopers World Congress",
+    image: "https://images.unsplash.com/photo-1540575467063-178a50c2df87?w=800",
+    slug: "wearedevelopers-2026",
+    location: "Berlin, Germany",
+    date: "July 14-16, 2026",
+    time: "8:00 AM - 7:00 PM",
+  },
+  {
+    title: "AWS re:Invent 2026",
+    image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa?w=800",
+    slug: "aws-reinvent-2026",
+    location: "Las Vegas, NV",
+    date: "November 30 - December 4, 2026",
+    time: "8:00 AM - 9:00 PM",
+  },
+  {
+    title: "React Summit US 2026",
+    image: "https://images.unsplash.com/photo-1633356122544-f134324a6cee?w=800",
+    slug: "react-summit-us-2026",
+    location: "New York, NY",
+    date: "June 15-16, 2026",
+    time: "9:30 AM - 6:00 PM",
+  },
+  {
+    title: "All Things Open 2026",
+    image: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?w=800",
+    slug: "all-things-open-2026",
+    location: "Raleigh, NC",
+    date: "October 25-27, 2026",
+    time: "9:00 AM - 6:30 PM",
+  },
+  {
+    title: "DevDays Europe 2026",
+    image: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?w=800",
+    slug: "devdays-europe-2026",
+    location: "Vilnius, Lithuania",
+    date: "May 20-22, 2026",
+    time: "8:30 AM - 7:00 PM",
+  },
+];
